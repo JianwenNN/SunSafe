cf3f1a0277163717a5197a3124a83404a60dfa5a
jxu737 <jxu737@gatech.edu>
2025-03-04 11:25:50 -0600

    Added completed version of implementation

--- index.js
+++ index.js
@@ -6,9 +6,38 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
+});
+
+app.post('/submit', async (req, res) => {
+    const response = await axios.get('https://api.openuv.io/api/v1/uv', {
+        headers: {
+            'x-access-token': 'openuv-11t2aerm7tv51k3-io',
+        },
+        params: {
+            'lat': req.body.latitude,
+            'lng': req.body.longitude,
+        }
+    })
+
+
+    if (response.data.result.uv > 3) {
+        res.render('index.ejs', {
+            imageURL: 'sunscreen.jpg',
+            urlStr: 'sunscreen',
+            suggestion: 'You need to use sunscreen now.',
+        })
+    }
+    else {
+        res.render('index.ejs', {
+            imageURL: 'cloudy.jpg',
+            urlStr: 'cloudy',
+            suggestion: 'You are safe to go out without sunscreen.',
+        });
+    }
 });
 
 app.listen(port, () => {
